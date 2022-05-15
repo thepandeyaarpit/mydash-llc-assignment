c17a65c2601fc2e4daeb419914d355ad5c679a0f
Arpit <69616138+thepandeyaarpit@users.noreply.github.com>
2022-05-15 23:56:19 +0530

    changes

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import Nav from './Nav'
 import '../App.css';
 
 const Main = () => {
+
+  const [data] = useState([150, 60, 200, 285, 173]);
+  const svgRef = useRef();
+
+  useEffect(() => {
+    // settingup svg container
+    const w = 400;
+    const h = 300;
+    const svg = d3.select(svgRef.current);
+
+    // setting the scaling
+
+    // setting the axes
+
+    // setting the svg data
+  }, [data]);
+
   return (
       <>
           <Nav />
-            <div>Main</div>
+            <div>
+            <svg ref={svgRef}></svg>
+            </div>
       </>
   )
 }
